e3476b5e5e7b40b6189bb454ef13a0925833b6c3
Giovanni Minoccari <98152727+Giominoccari@users.noreply.github.com>
2026-04-13 22:12:00 +0200

    Update entrypoint.swift

--- Sources/App/entrypoint.swift
+++ Sources/App/entrypoint.swift
@@ -7,9 +7,7 @@ enum Entrypoint {
     static func main() async throws {
         var env = try Environment.detect()
 
-        let workDir = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
-        let logsDir = workDir.appendingPathComponent("Storage/logs")
-        LoggingSystem.bootstrapFunghi(logsDir: logsDir, env: env)
+        try LoggingSystem.bootstrap(from: &env)
 
         let app = try await Application.make(env)
         defer { Task { try await app.asyncShutdown() } }
